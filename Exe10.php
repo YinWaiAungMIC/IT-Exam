a212d0ffe2d91b0e0d9371d866a9c20cbb84224b
YinWaiAungMIC <yinwaiaungcsf1@gmail.com>
2020-07-02 17:14:34 +0630

    IT Exam

---
+++ Exe10.php
@@ -0,0 +1,14 @@
+<?php
+
+ $burger=4.95;
+ $cmilk=1.95;
+ $cola=85/100;
+ $t=$burger+$cmilk+$cola;
+ $total=$t+($t*7.5)+($t*16);
+
+
+echo 'Total Cost '.$total.'<br>';
+
+
+
+?>
---
+++ Exe11.php
@@ -0,0 +1,17 @@
+<?php
+    echo "<table border='1'>";
+    	echo "<tr>";
+    	echo "<td>".'Salary of Mr.A is'."</td>";
+    	echo "<td>".'10'."</td>";
+    	echo "</tr>";
+    	echo "<tr>";
+    	echo "<td>".'Salary of Mr.B is'."</td>";
+    	echo "<td>".'12'."</td>";
+    	echo "</tr>";
+    	echo "<tr>";
+    	echo "<td>".'Salary of Mr.C is'."</td>";
+    	echo "<td>".'14'."</td>";
+    	echo "</tr>";
+    	
+    echo"</table>";
+    ?>
